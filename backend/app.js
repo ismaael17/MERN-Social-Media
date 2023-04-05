36e6a4e948b2dd574ad1c72cc43f68505e137a9f
ISMAA'EL MUHAMMAD IBRAHIM Khodabucchas <24378437@sun.ac.za>
2023-04-05 11:04:22 +0400

    cors for backend

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/users");
 const postRoutes = require("./routes/posts");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 connectDB();
 
